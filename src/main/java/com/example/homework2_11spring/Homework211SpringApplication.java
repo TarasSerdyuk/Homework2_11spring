eba14152618db6e5da254e8f3d1d4f008a06325f
Тарас <Acroyd1984@gmail.com>
2023-07-18 21:10:55 +0300

    Start

---
+++ src/main/java/com/example/homework2_11spring/Homework211SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework2_11spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework211SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework211SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework2_11spring/Homework211SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework2_11spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework211SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
